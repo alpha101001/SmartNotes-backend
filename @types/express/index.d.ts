98dc2a23a7e4bd248df79f8679a87ecd6f7faf87
Avishek Roy <avishek.1807053@gmail.com>
2024-12-25 18:44:10 +0600

    types added 2

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -5,8 +5,15 @@ declare global {
     interface Request {
       user?: {
         _id: string;
-        email: string;
+        email?: string;
       };
+      body?: {
+         name?: string;
+         email?: string;
+         password?: string;
+         otp?: string;
+         newPassword?: string;
+       };
     }
   }
 }
