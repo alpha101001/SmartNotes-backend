e1b5958e37d31ebe1affadd595f66648f2f10ebd
Avishek Roy <avishek.1807053@gmail.com>
2024-12-25 08:32:54 +0600

    cors error for vercel domain fixed with proper string

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(cors(
    {
-      origin: 'https://smart-notes-deployment-1.vercel.app/',
+      origin: 'https://smart-notes-deployment-1.vercel.app',
       credentials: true,
    }
 ));
